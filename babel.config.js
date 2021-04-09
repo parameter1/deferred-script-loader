54002dc1891debcbe4e97fb57182d72b3564542c
Jacob Bare <jacob@parameter1.com>
2021-04-09 09:19:30 -0500

    Upgrade dependencies

--- babel.config.js
+++ babel.config.js
@@ -14,14 +14,14 @@ module.exports = (api) => {
           ios: '10',
         },
         useBuiltIns: 'usage',
-        corejs: '3.9',
+        corejs: '3.10',
         debug: false,
       },
     ],
   ];
 
   const plugins = [
-    ['@babel/plugin-transform-runtime', { version: '^7.13.10' }],
+    ['@babel/plugin-transform-runtime', { version: '^7.13.15' }],
   ];
 
   return {
